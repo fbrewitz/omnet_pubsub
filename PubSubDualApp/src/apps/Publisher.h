00ab46d3abc65eb6e493f31c960a7db330712acb
Filippa Brewitz <71699180+fbrewitz@users.noreply.github.com>
2026-05-26 12:32:38 +0200

    Refactor Publisher class to PublisherApp and clean up
    Renamed Publisher class to PublisherApp and removed unused parameters and commented code.

--- PubSubDualApp/src/apps/Publisher.h
+++ PubSubDualApp/src/apps/Publisher.h
@@ -6,18 +6,10 @@
 
 namespace pub {
 
-class Publisher : public inet::UdpBasicApp
+class PublisherApp : public inet::UdpBasicApp
 {
 protected:
     enum SelfMsgKinds { START = 1, SEND, STOP, ENCODE };
-
-    //parameters here
-    //int publisherId;
-    //int dataSetWriterId;
-    //double publisherInterval;
-    //int writerGroupId; // include this??
-    //double writerGroupExecTime;
-    //int messageLength;
 
     double lastEncodingExecTime;
     double publishedDataSet;
@@ -25,16 +17,7 @@ protected:
     double writerGroup;
     double processingSendTime;
 
-    int seqNr;
-
     omnetpp::simtime_t startTimePublish;
-    //omnetpp::simtime_t publishTime;
-    //omnetpp::simtime_t creationTime;
-    //omnetpp::simtime_t subTime;
-    //omnetpp::simtime_t delta;
-
-private:
-    //omnetpp::simsignal_t deltaSignal;
 
 protected:
     // override methods here
@@ -42,13 +25,9 @@ protected:
     virtual void initialize(int stage) override;
     virtual void finish() override;
     virtual void sendPacket() override;
-    //virtual void processPacket(inet::Packet *pk) override;
     virtual void handleMessageWhenUp(inet::cMessage *msg) override;
     virtual void processStart() override;
     virtual void processSend() override;
-    //virtual void setSocketOptions() override;
-    //virtual void socketDataArrived(inet::UdpSocket *socket, inet::Packet *packet) override;
     void processEncoding();
-
 };
 }
