b53f4f77845f0bb5c11d20b0dafc9a00e2e35a8f
Filippa Brewitz <71699180+fbrewitz@users.noreply.github.com>
2026-05-26 12:31:35 +0200

    Rename PublisherSafety to PublisherEchoApp

--- PubSubDualApp/src/apps/PublisherEcho.h
+++ PubSubDualApp/src/apps/PublisherEcho.h
@@ -6,40 +6,17 @@
 
 namespace pub {
 
-class PublisherSafety : public inet::UdpBasicApp
+class PublisherEchoApp : public inet::UdpBasicApp
 {
 protected:
     enum SelfMsgKinds { START = 1, SEND, STOP, ENCODE };
-
-    //parameters here
-    //int publisherId;
-    //int dataSetWriterId;
-    //double publisherInterval;
-    //int writerGroupId; // include this??
-    //double writerGroupExecTime; // do I use this?
-    //int messageLength;
-
     double lastEncodingExecTime;
     double publishedDataSet;
     double dataSetWriter;
     double writerGroup;
     double processingSendTime;
 
-    //int numFields; // include this?
-    //int keyFrameCount; //include this?
-    //double keepAliveTime; // include this?
     omnetpp::simtime_t publishTime = 0;
-    omnetpp::simtime_t creationTime;
-    omnetpp::simtime_t subTime;
-    omnetpp::simtime_t delta;
-
-    int seqNr = 0;
-
-private:
-    omnetpp::simsignal_t deltaSignal;
-
-//public:
-//    virtual void processPacket(inet::Packet *pk) override; // so that it can be accessed by the Subscriber
 
 protected:
     // override methods here
